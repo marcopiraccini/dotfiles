61773a968ec2365346810b9d82b4b6ab8227c458
marcopiraccini <marco.piraccini@gmail.com>
2024-06-10 15:11:53 +0200

    updated types
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
+git pull origin master
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "setup.sh" \
-		--exclude "README.md" \
+		--exclude "README.MD" \
 		--exclude "LICENSE" \
 		--exclude "doc" \
-		-avh --no-perms . ~;
+		-avh --no-perms . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		doIt
+	fi
+fi
+unset doIt
