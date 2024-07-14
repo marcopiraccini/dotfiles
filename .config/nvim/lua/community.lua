dbf6f3fceefd0f050477d7e3000fd8434c0d4531
marcopiraccini <marco.piraccini@gmail.com>
2024-07-14 08:47:00 +0200

    lint fixup

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -8,7 +8,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.nordic-nvim" },
